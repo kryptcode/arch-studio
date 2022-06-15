8ba4465fd9acdda375461e8369a7b277c80d9441
kryptcode <54333700+kryptcode@users.noreply.github.com>
2022-06-15 17:45:24 +0100

    Fixed visual bug

--- components/About/Banner.js
+++ components/About/Banner.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 const Banner = () => {
   return (
-    <div className='w-[80%] mx-auto flex flex-col-reverse space-y-8 md:space-y-0 md:flex-row space-x-0 md:space-x-12 mt-40'>
-        <div className='flex-1 flex flex-col justify-between'>
+    <div className='w-full md:w-[80%] mx-auto flex flex-col-reverse space-y-8 md:space-y-0 md:flex-row space-x-0 md:space-x-12 mt-40'>
+        <div className='flex-1 flex flex-col justify-between px-2 md:px-0'>
             <div className='h-[1px] w-16 bg-gray-400  ' />
-            <h2 className='text-[5rem] leading-tight font-bold'>
+            <h2 className='text-4xl md:text-[5rem] leading-tight font-bold'>
                 Our <br /> Heritage
             </h2>
             <div className='space-y-5 font-medium'>
